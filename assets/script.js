24d780cc3538634590ec26d1116c7edd250ee75b
Joe Fitzgerald <jfitzgerald@veritone.com>
2021-07-16 22:57:18 -0700

    updated javascript to include current weather fetch and saved searches

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,90 @@
 var searchFormEl = document.querySelector('#search-form');
+var inputEl = document.querySelector('#search-input');
+var previousSearchesEl = document.querySelector('#previous-searches');
 var cityDateEl = document.querySelector('#city-date');
+var currentTempEl = document.querySelector('#current-temp');
+var currentWindEl = document.querySelector('#current-wind');
+var currentHumidityEl = document.querySelector('#current-humidity');
+var currentUvEl = document.querySelector('#current-uv');
+var searchList = JSON.parse(localStorage.getItem("Search History")) || [];
+var forecastEl = document.querySelector('#forecast');
 
 function handleSearchFormSubmit(event) {
   event.preventDefault();
 
-  var cityQuery = document.querySelector('#search-input').value;
+  var cityQuery = inputEl.value;
 
   if (!cityQuery) {
     console.error('You must enter a city name.');
     return;
   }
 
-//   var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
-
-  // location.assign(queryString);
-
   searchApi(cityQuery);
+  saveCitySearch(cityQuery);
 }
 
 function searchApi(cityQuery) {
-  var weatherQueryUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityQuery + '&appid=208d873c4b4df3505fa91990e6501772';
+  var currentWxUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityQuery + '&units=imperial&appid=208d873c4b4df3505fa91990e6501772';
   var today = moment();
 
-  fetch(weatherQueryUrl)
+  fetch(currentWxUrl)
     .then(function (response) {
       if (!response.ok) {
         throw response.json();
       }
       return response.json();
     })  
-    .then(function (cityWxResults) {
-      console.log(cityWxResults);
-      var wxIcon = cityWxResults.weather[0].icon;
+    .then(function (currentWx) {
+      console.log(currentWx);
+
+      var wxIcon = currentWx.weather[0].icon;
       var wxIconSource = "http://openweathermap.org/img/wn/" + wxIcon + "@2x.png";
       var img = document.createElement('img');
       img.src = wxIconSource;
       cityDateEl.textContent = cityQuery + ' ' + today.format("L");
       cityDateEl.appendChild(img);
-    })
+
+      currentTempEl.textContent = "Temp: " + currentWx.main.temp + '°F';
+      currentWindEl.textContent = "Wind: " + currentWx.wind.speed + ' MPH';
+      currentHumidityEl.textContent = "Humidity: " + currentWx.main.humidity + '%';
+      
+      var lat = currentWx.coord.lat;
+      var long = currentWx.coord.lon;
+      var forecastWxUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&exclude=minutely,hourly,alerts&appid=208d873c4b4df3505fa91990e6501772";
+  
+      fetch(forecastWxUrl)
+        .then (function (response) {
+          return response.json();
+        })
+        .then(function (currentUV) {
+          var cityUv = currentUV.current.uvi;
+          currentUvEl.textContent = "UV Index: " + cityUv;
+        })
+      })
+
+      //create h3 element that reads 5-Day Forecast
+      //give it textContent
+      //append it to forecast
+      //for loop 
+
     .catch(function(error) {
       console.error(error);
     });
+
+}
+
+function saveCitySearch (cityQuery) {
+  //city name is saved to local storage, button element is created with city name, button has addEventListener to run with city name passed into it
+  searchList.push(cityQuery);
+  localStorage.setItem("Search History", JSON.stringify(searchList));
+  var citySearchEl = document.createElement("button");
+  citySearchEl.setAttribute("type", "button");
+  citySearchEl.setAttribute("value", cityQuery);
+  citySearchEl.setAttribute("class", "city-button");
+  previousSearchesEl.append(citySearchEl);
 }
 
 searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+
+//need an onload function to render previous city searches from local storage
 
